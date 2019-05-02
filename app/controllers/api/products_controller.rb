a02fab81a678f46fc28b4ea5c1d92ce530413d15
robocop019 <chrisbowen@comcast.net>
2019-05-02 10:22:39 -0500

    added more routes, controller code, and views

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,19 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render 'all_products_view.json.jbuilder'
   end
+
+  def godzilla_action
+    @product = Product.find(1)
+    render 'godzilla_view.json.jbuilder'
+  end
+
+  def ghidorah_action
+    @product = Product.find(2)
+    render 'ghidorah_view.json.jbuilder'
+  end
+
+  def rodan_action
+    @product = Product.find(3)
+    render 'rodan_view.json.jbuilder'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/all_products_url' => 'products#all_products_action'
+    get '/godzilla_url' => 'products#godzilla_action'
+    get '/ghidorah_url' => 'products#ghidorah_action'
+    get '/rodan_url' => 'products#rodan_action'
   end
 end
